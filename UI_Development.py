fb6be7918841b43f25c9db3cea0c1bf139c518ee
Andrei-Ioan Debu <andreidebu13@icloud.com>
2023-03-15 08:49:46 +0200

    Showing the selected image in the UI

--- UI_Development.py
+++ UI_Development.py
@@ -4,47 +4,72 @@ from PyQt6.QtCore import *
 import sys
 
 
-class Window(QMainWindow):
+class Window(QDialog):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Test UI")
+        self.setWindowTitle("Artificial Intelligence")
         self.view = QListWidget()
         self.setGeometry(100, 100, 800, 600)
         self.setWindowIcon(QIcon("AI.jpg"))
-        self.UIComponents(text)
+
+        vbox = QVBoxLayout()
+        addButton = QPushButton("Browse image")
+        addButton.clicked.connect(self.browseImage)
+        vbox.addWidget(addButton)
+
+        self.label = QLabel(text)
+        vbox.addWidget(self.label)
+
+        self.setLayout(vbox)
+
         self.show()
 
+    def browseImage(self):
+        fileName = QFileDialog.getOpenFileName(self, 'Select image', 'c\\', 'Images (*.png *.jpg *.jpeg)')
+
+        imagePath = fileName[0]
+        pixmap = QPixmap(imagePath)
+        self.label.setPixmap(QPixmap(pixmap))
+        self.resize(pixmap.width(), pixmap.height())
+
     def UIComponents(self, printText):
-        addButton = QPushButton("Add image(s)", self)
-        addButton.setGeometry(30, 30, 100, 30)
-        addButton.clicked.connect(self.clickToAddPhoto)
+        myFont = QFont()
+        myFont.setBold(True)
         label = QLabel(printText, self)
-        label.move(150, 30)
+        label.setFont(myFont)
+        label.move(150, 25)
 
-    def clickToAddPhoto(self):
-        paths, _ = QFileDialog.getOpenFileName(self, 'Select image(s)', '', 'Images (*.png *,jpg *jpeg)')
-        size = self.view.iconSize()
-        for path in paths:
-            source = QPixmap(path)
-            if source.isNull():
-                continue
-            if source.width() > size.width() or source.height() > size.height:
-                source = source.scaled(size)
-            square = QPixmap(size)
-            qp = QPainter(square)
-            rect = source.rect()
-            rect.moveCenter(square.rect().center())
-            qp.drawPixmap(rect, source)
-            qp.end()
+        vbox = QVBoxLayout()
+        addButton = QPushButton("Browse image", self)
+        addButton.setGeometry(30, 20, 100, 30)
+        addButton.clicked.connect(self.browseImage)
 
-            name = QFileInfo(path).baseName()
-            item = QListWidgetItem(name)
-            item.setIcon(QIcon(square))
-            item.setToolTip(path)
-            item.setSizeHint(size)
-            self.view.addItem(item)
-            return self.view
+        vbox.addWidget(addButton)
+
+    # def clickToAddPhoto(self):
+    #     paths, _ = QFileDialog.getOpenFileName(self, 'Select image(s)', '', 'Images (*.png *,jpg *jpeg)')
+    #     size = self.view.iconSize()
+    #     for path in paths:
+    #         source = QPixmap(path)
+    #         if source.isNull():
+    #             continue
+    #         if source.width() > size.width() or source.height() > size.height:
+    #             source = source.scaled(size)
+    #         square = QPixmap(size)
+    #         qp = QPainter(square)
+    #         rect = source.rect()
+    #         rect.moveCenter(square.rect().center())
+    #         qp.drawPixmap(rect, source)
+    #         qp.end()
+    #
+    #         name = QFileInfo(path).baseName()
+    #         item = QListWidgetItem(name)
+    #         item.setIcon(QIcon(square))
+    #         item.setToolTip(path)
+    #         item.setSizeHint(size)
+    #         self.view.addItem(item)
+    #         return self.view
 
 
 text = "Print test"
