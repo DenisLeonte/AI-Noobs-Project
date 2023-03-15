49e37d75edbf0288ffef478f228d3c554db40712
Andrei-Ioan Debu <andreidebu13@icloud.com>
2023-03-15 07:55:57 +0200

    Print text

--- UI_Development.py
+++ UI_Development.py
@@ -4,38 +4,22 @@ from PyQt6.QtCore import *
 import sys
 
 
-# class ImageListView(QWidget):
-#     def __init__(self):
-#         super().__init__()
-#
-#         self.view = QListWidget()
-#         self.view.setIconSize(QSize(64, 64))
-#         bigFont = self.font()
-#         bigFont.setPointSize(24)
-#         self.view.setFont(bigFont)
-#
-#         self.addButton = QPushButton('Add image(s)')
-#
-#         layout = QVBoxLayout(self)
-#         layout.addWidget(self.view)
-#         layout.addWidget(self.addButton)
-#         self.addButton.clicked.connect(self.addImage)
-#         self.addButton.clicked(self.addImage)
-
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Test UI")
         self.view = QListWidget()
-        self.setGeometry(100, 100, 600, 400)
-        self.UIComponents()
+        self.setGeometry(100, 100, 800, 600)
+        self.UIComponents(text)
         self.show()
 
-    def UIComponents(self):
+    def UIComponents(self, text):
         addButton = QPushButton("Add image(s)", self)
-        addButton.setGeometry(200, 150, 100, 30)
+        addButton.setGeometry(30, 30, 100, 30)
         addButton.clicked.connect(self.clickToAddPhoto)
+        label = QLabel(text, self)
+        label.move(150, 30)
 
     def clickToAddPhoto(self):
         paths, _ = QFileDialog.getOpenFileName(self, 'Select image(s)', '', 'Images (*.png *,jpg *jpeg)')
@@ -61,6 +45,7 @@ class Window(QMainWindow):
             self.view.addItem(item)
 
 
+text = "Print test";
 App = QApplication(sys.argv)
 window = Window()
 sys.exit(App.exec())
